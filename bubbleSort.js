2459602c6de90b82e4521538e8f016682555f2ab
mostofakabir <76174777+mostofakabir@users.noreply.github.com>
2021-08-11 22:18:48 +0600

    bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,12 +1,17 @@
 function bubbleSort(array) {
     let i, j, temp;
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length - 1; i++) {
+        swap = false;
         for (let j = 0; j < array.length - i - 1; j++) {
             if (array[j] > array[j + 1]) {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                swap = true;
             }
+        }
+        if (swap == false) {
+            break;
         }
     }
     return array;
